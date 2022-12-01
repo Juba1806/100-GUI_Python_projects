916bc13ee127206dab4b54361c775eeb546cca2f
Juba1806 <elaissaouimohammed01@gmail.com>
2022-12-01 03:19:55 +0000

    Add something

--- 1_Begles/bagels.py
+++ 1_Begles/bagels.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python3
 
-print("\nBagels, a deductive logic game.") 
-print("\n\n\n")
-
-print("I am thinking of a 3-digit number. Try to guess what it is.")
-print("Here are some clues:\n")
-
-print("When I say:    That means:")
-print("\n Pico\t\tOne digit is correct but in the wrong position.")
-print(" Fermi\t\tOne digit is correct and in the right position.")
-print(" Bagels\t\tNo digit is correct.")
-
-print("\nI have thought up a number.")
-print(" You have 10 guesses to get it.")
+NUM_DIGITS = 3 # (!) Try setting this to 1 or 10.
+MAX_GUESSES = 10 # (!) Try setting this to 1 or 100. 
 
 
-x = 1 
-y = 3 
-z = 8 
+def main():
+    print(""" 
+I am thinking of a {}-sigit number with no repeated digits. 
+Try to guess what it is. Here are some clues:
+When I say:    That means:
+ Pico          One digit is correct but in the wrong position. 
+ Fermi         One digit is correct and in the right position. 
+ Walo          No digit is correct. 
 
-for a in range(1,10):
-    number = input(f"Guess #{a}  ")
-    number = x + y + z 
+for example, if the secret number was 248 and your guess was 843, the 
+clues would be Fermi Pico. """.format(NUM_DIGITS))
 
-    if number == x or number == y or number == z:
-        print("Pico")
-    elif number== x and number == y or number == y and number == z or number == z and x:
-        print("Pico Pico")
-    elif number == x and number == y and  number == z : 
-        print("you got this")
-    else:
-        print("no you wrong")
