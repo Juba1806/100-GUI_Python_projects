29bb16095c532986660fc76efe8318cf20e3c859
Juba1806 <elaissaouimohammed01@gmail.com>
2022-12-19 07:21:46 +0100

    adding something

--- 2_The_Birthday_Paradox/Birthday_Paradox.py
+++ 2_The_Birthday_Paradox/Birthday_Paradox.py
@@ -2,5 +2,17 @@
 
 import datetime, random 
 
+def getBirthdays(numberOfBirthdays):
+    """ Returns a list of number random date objects for birthdays."""
+    birthdays = []
 
-print("hello world")
+    for i in range(numberOfBirthdays):
+        # The year is unimportant for our simulation, as long as all 
+        startOfYear = datetime.date(2000,1,1)
+
+        # Get a random day into the year:
+        randomNumberOfDays = datetime.timedelta(random.randint(0,364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+
+    return birthdays
