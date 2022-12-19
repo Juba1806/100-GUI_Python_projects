5b604b0a1bcb876b8646f6f459e3c52d6d95f76a
Juba1806 <elaissaouimohammed01@gmail.com>
2022-12-19 07:26:27 +0100

    add another funtion

--- 2_The_Birthday_Paradox/Birthday_Paradox.py
+++ 2_The_Birthday_Paradox/Birthday_Paradox.py
@@ -16,3 +16,14 @@ def getBirthdays(numberOfBirthdays):
         birthdays.append(birthday)
 
     return birthdays
+
+def getMatch(birthdays):
+    """ Returns the date object of a birthday that occurs more than once in the birthdays list."""
+    if len(birthdays) == len(set(birthdays)):
+        return None # All birthedays are unique, so return None.
+
+    # Compare each birthday to every other birthday:
+    for a , birthdayA in enumerate(birthdays):
+        for b, birthdayB in enumerate(birthdays[ a + 1 :]):
+            if birthdayA == birthdayB:
+                return birthdayA # Return thematching birthday.
