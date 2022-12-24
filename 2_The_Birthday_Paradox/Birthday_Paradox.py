9bbe323c8cab4e1d0b2eabb027cb60dbf199419a
Juba1806 <elaissaouimohammed01@gmail.com>
2022-12-24 07:24:36 +0100

    add stuff

--- 2_The_Birthday_Paradox/Birthday_Paradox.py
+++ 2_The_Birthday_Paradox/Birthday_Paradox.py
@@ -1,29 +1,46 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
+
 
 import datetime, random 
 
-def getBirthdays(numberOfBirthdays):
-    """ Returns a list of number random date objects for birthdays."""
+
+def getBirthdays(number):
+    """ Returns a list of random birthdays """
+    
     birthdays = []
 
-    for i in range(numberOfBirthdays):
-        # The year is unimportant for our simulation, as long as all 
-        startOfYear = datetime.date(2000,1,1)
+    # The start of the year and the year is unimportant for our simulation
+    startday = datetime.date(2000, 1, 1)
 
-        # Get a random day into the year:
-        randomNumberOfDays = datetime.timedelta(random.randint(0,364))
-        birthday = startOfYear + randomNumberOfDays
-        birthdays.append(birthday)
+    for i in range(number):
+    # Generate ramdom birthdays from the year 
+        
+
+        #random day from the year 
+        randomday = datetime.timedelta(random.randint(0,365))
+
+        Birthday = startday + randomday
+        birthdays.append(Birthday)
 
     return birthdays
 
-def getMatch(birthdays):
-    """ Returns the date object of a birthday that occurs more than once in the birthdays list."""
-    if len(birthdays) == len(set(birthdays)):
-        return None # All birthedays are unique, so return None.
+nn = getBirthdays(25)
 
-    # Compare each birthday to every other birthday:
-    for a , birthdayA in enumerate(birthdays):
-        for b, birthdayB in enumerate(birthdays[ a + 1 :]):
-            if birthdayA == birthdayB:
-                return birthdayA # Return thematching birthday.
+print(nn)
+
+
+
+
+
+
+
+
+def haja(nn):
+
+    if len(nn) == len(set(nn)):
+        return " iwa nn"
+
+    elif len(nn) != len(set(nn)):
+         return " Ahh a sat kayn"
+
+print(haja(nn))
