fa96f1eb591243a5b118ade44fa5be65560996c7
Juba1806 <elaissaouimohammed01@gmail.com>
2022-12-26 15:03:58 +0100

    delete

--- haja/haja.c
+++ haja/haja.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	int birth = 2003;
-	int now = 2022;
-	int age = now - birth;
-	printf("My age now is %d", age);
-	printf("\n");
-	printf("and I am dong good");
-	printf("\n");
+	printf("Testing C");
 	return 0;
 }
 
