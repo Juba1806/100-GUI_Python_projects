a04f9167b56760bed5cffa42c48c3510ce1ecba2
Juba1806 <elaissaouimohammed01@gmail.com>
2023-04-09 19:43:02 +0000

    Some changes to the design

--- 03_Digital_Clock_GUI/clock.py
+++ 03_Digital_Clock_GUI/clock.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 
-from tkinter import Label, Tk
+import tkinter as tk
 import time
 
+def update_time():
+    """ A funtion a update time """
+    current_time = time.strftime('%H:%M:%S')
+    clock_label.config(text=current_time)
+    clock_label.after(1000, update_time)
 
+# Open the window 
+root = tk.Tk()
+# Title for the app
+root.title("Digital Clock")
 
-app_window = Tk()
-app_window.title("Digital Clock")
-app_window.geometry("420x150")
-app_window.resizable(1,1)
+# Edit some changes   
+clock_label = tk.Label(root, font=('Arial', 100), bg='#222c2c', fg='white')
+clock_label.pack(fill='both', expand=True)
 
-text_font= ("Boulder", 68, 'bold')
-background = "#f2e750"
-foreground= "#363529"
-border_width = 25
+# Update time accordingly 
+update_time()
 
-label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width)
-label.grid(row=0, column=1)
-
-def digital_clock():
-   time_live = time.strftime("%H:%M:%S")
-   label.config(text=time_live)
-   label.after(200, digital_clock)
-
-digital_clock()
-app_window.mainloop()
+root.mainloop()
 
